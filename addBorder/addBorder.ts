a3fb79bbb85b780693f7ad18a89c90f72531e144
Larocca <Larocca@RIVPRO1.net>
2022-09-29 16:16:18 -0400

    Solved #12

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,21 @@
-export function addBorder(picture: string[]): string[] {
-
+export function addBorder(picture: string[]): any {
+    let borderStars: string = ``;
+    let stringArray: string[] = [];
+  
+    for(let i = 0; i < picture[0].length + 2; i++) {
+      borderStars += '*';
+    }
+  
+    stringArray.push(borderStars);
+  
+    for(let i = 0; i < picture.length; i++) {
+      stringArray.push(`*${picture[i]}*`);
+    }
+  
+    stringArray.push(borderStars);
+        console.log(borderStars.length);
+        console.log(stringArray); 
+    return stringArray;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
