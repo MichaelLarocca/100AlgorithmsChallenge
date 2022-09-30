b7257eca271db1bb4a6173883bfa7da14e96d3b0
Larocca <Larocca@RIVPRO1.net>
2022-09-30 12:16:13 -0400

    Solved #13

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,22 @@
+
 export function addTwoDigits(n: any): number {
 
+    const numberToString = n.toString();
+            console.log(typeof numberToString)
+
+    const numberArray = numberToString.split('');
+            console.log(numberArray)
+
+    const numberOne = parseInt(numberArray[0]);
+            console.log(typeof numberOne)
+
+    const numberTwo = parseInt(numberArray[1]);    
+            console.log(typeof numberTwo)
+
+    const total = numberOne + numberTwo; 
+            console.log(total); 
+
+    return total;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
