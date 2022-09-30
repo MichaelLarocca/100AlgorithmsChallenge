03a27ef9beb58d1b4750ea1b1eb41abca651018c
Larocca <Larocca@RIVPRO1.net>
2022-09-30 13:04:26 -0400

    Solved adjacent Elements Product

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,19 @@
-export function adjacentElementsProduct(inputArray: number[]): number {
 
+export function adjacentElementsProduct(inputArray: number[]): number {
+    
+    let largest: number = 0;
+
+    for(let i = 0; i < inputArray.length - 1; i++) {
+                    console.log(inputArray[i]);
+
+        if(inputArray[i] * inputArray[i+1] > largest) {
+            
+            largest = inputArray[i] * inputArray[i+1];
+                    console.log(largest)
+        }
+    } 
+
+    return largest;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
