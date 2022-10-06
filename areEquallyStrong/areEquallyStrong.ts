cfa1642c8eae9994a5eb25c174a5dc97904087a0
Larocca <Larocca@RIVPRO1.net>
2022-10-06 11:47:14 -0400

    Solved Are Equally Strong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,13 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
 
+    let yourStrength: number = yourLeft <= yourRight ? yourLeft : yourRight;
+            console.log(yourStrength);
+    let yourFriendsStrength: number = friendsLeft <= friendsRight ? friendsLeft: friendsRight;
+            console.log(yourFriendsStrength)
+
+    return yourStrength === yourFriendsStrength ? true : false;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
