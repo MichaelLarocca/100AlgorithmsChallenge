60e2d97f2b9bab938a08bfd12d98fafeafb2dd16
Larocca <Larocca@RIVPRO1.net>
2022-10-11 12:29:18 -0400

    Solved Array Maximal Adjacent Difference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,15 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+    
+    let maxAdjacentDifference: number = 0;
 
+    for(let i = 0; i < inputArray.length -1; i++) {
+                console.log(inputArray[i] - inputArray[i+1]);
+        if(inputArray[i] - inputArray[i+1] > maxAdjacentDifference) {
+            maxAdjacentDifference = inputArray[i] - inputArray[i+1];
+        }        
+    }
+                console.log(maxAdjacentDifference);
+    return maxAdjacentDifference;
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
