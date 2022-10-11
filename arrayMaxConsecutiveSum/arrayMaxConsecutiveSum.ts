4ce5aa303658531abd44098deefeb7c09d2ab5fe
Larocca <Larocca@RIVPRO1.net>
2022-10-11 12:07:01 -0400

    Solved Array Max Consecutive Sum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,14 @@
 export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+    const arrayOfSums: number[] = [];
 
+    for(let i = 0; i < inputArray.length -1; i++) {
+            console.log(inputArray[i] + inputArray[i+1]);
+
+        arrayOfSums.push(inputArray[i] + inputArray[i+1]);
+    }
+        console.log(Math.max(...arrayOfSums))
+        
+    return Math.max(...arrayOfSums);
 }
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
