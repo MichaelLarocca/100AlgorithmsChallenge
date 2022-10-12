5ae9de94b364902d8250ee5306f9ef261e104bfb
Larocca <Larocca@RIVPRO1.net>
2022-10-12 13:35:50 -0400

    Solved Array Previous Less

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,18 @@
 export function arrayPreviousLess(items: number[]): number[] {
 
+    for(let i = items.length; i > 0 ; i--) {
+
+                console.log(items[i-1]);
+                console.log(items[i-2]);
+
+        if(items[i-2] < items[i-1]) {
+            items[i-1] = items[i-2];
+        } else {
+            items[i-1] = -1;
+        }
+    }
+         console.log(items);
+    return items;
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
