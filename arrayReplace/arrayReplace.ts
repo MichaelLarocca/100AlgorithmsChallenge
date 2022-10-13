a5d5867d2087887269f484f279c718fb966a4736
Larocca <Larocca@RIVPRO1.net>
2022-10-13 12:47:34 -0400

    Solved Array Replace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,17 @@
 export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
 
+    for(let i = 0; i < inputArray.length; i++) {
+                console.log(inputArray[i]);
+                console.log(elemToReplace);
+                console.log(substitutionElem);
+
+        if(inputArray[i] === elemToReplace) {
+            inputArray[i] = substitutionElem;
+        }
+        
+    }
+        console.log(inputArray)
+    return inputArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
