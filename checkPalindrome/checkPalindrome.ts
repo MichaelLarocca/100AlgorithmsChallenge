71e0f45c2fea8836b7af4a38496ed18049b91200
Larocca <Larocca@RIVPRO1.net>
2022-10-15 11:17:01 -0400

    Solved Check Palindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,14 @@
 export function checkPalindrome(inputString: string): boolean {
+    
+    let string: string = inputString.toLocaleLowerCase();
+    let stringReverse: string = inputString.toLocaleLowerCase().split('').reverse().join('');
 
+        console.log(string);
+        console.log(stringReverse);
+
+    return string === stringReverse;   
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome('aabaa'));
+console.log(checkPalindrome('abac'));
+console.log(checkPalindrome('a'));
