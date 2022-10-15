b8df05716f7eec78e87aeaea9e642f28320810a8
Larocca <Larocca@RIVPRO1.net>
2022-10-15 09:35:09 -0400

    Solved Case Insensitive Palimdrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,9 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
+    
+            console.log(inputString.split("").reverse().join("").toLowerCase()); 
 
+    return inputString.toLowerCase() === inputString.split("").reverse().join("").toLowerCase();
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abac'));
