f3f5fd4250fdd77aae7deadb3d1dc02e941006a3
Larocca <Larocca@RIVPRO1.net>
2022-10-15 10:54:22 -0400

    Solved Character Parity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,22 @@
 export function characterParity(symbol: string): string {
+    
+    let result: string = "";
+    const input: number = parseInt(symbol);
 
+            console.log(input);
+
+    result = input % 2 === 0 ? "even" : "odd";   
+            
+    if(isNaN(input)) {
+
+            console.log(isNaN(input));
+
+        result = "not a digit";
+    }
+
+    return result;
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity('5'))
+console.log(characterParity('8'))
+console.log(characterParity('q'))
