61a4deb8f9a4b26ffa4ddebc175e43dd1ccf1b69
Larocca <Larocca@RIVPRO1.net>
2022-10-15 09:52:24 -0400

    Solved Century From Year

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,11 @@
 export function centuryFromYear(year: number): number {
 
+            console.log(Math.ceil(year/100));
+
+    let century = (Math.ceil(year/100));
+
+    return century;
 }
 
-// console.log(centuryFromYear(1905));
-// console.log(centuryFromYear(1700));
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
