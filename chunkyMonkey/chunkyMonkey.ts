76d1290001cf4a6f1c97a926c965d9b564c04c27
Larocca <Larocca@RIVPRO1.net>
2022-10-17 12:31:29 -0400

    Solved Chunky Monkey

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,18 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
+    const returnArray: any[] = [];
 
+    const arrayOne: any[] = arr.slice(0, size);
+    const arrayTwo: any[] = arr.slice(size);
+
+            console.log(arrayOne);
+            console.log(arrayTwo);
+
+    returnArray.push(arrayOne); 
+    returnArray.push(arrayTwo);       
+    
+            console.log(returnArray);
+    return returnArray;
 }
 
-// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-// console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
