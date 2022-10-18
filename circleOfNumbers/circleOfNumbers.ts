4c078bffdd1290223bc787bd35cc0138c076ee48
Larocca <Larocca@RIVPRO1.net>
2022-10-18 17:45:06 -0400

    Solved Circle Of Numbers

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,21 @@
 export function circleOfNumbers(n: number, firstNumber: number): number {
 
+    let counter: number = firstNumber;
+
+    let distance: number = n / 2;
+            console.log(distance);
+    
+    for(let i = 0; i < distance; i ++) {
+
+        if(counter === n) {
+            counter = 0;
+        }
+        
+        counter += 1;
+    }        
+
+        console.log(counter);
+    return counter;        
 }
 
-// console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 2));
