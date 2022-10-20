4a7ae3b5f9b76f051871e9732416da6508cfeb50
Larocca <Larocca@RIVPRO1.net>
2022-10-20 14:05:34 -0400

    Solved Compare Integers

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,20 @@
 export function compareIntegers(a: string, b: string): string {
 
+    const numOne = parseInt(a);
+            console.log(typeof numOne);
+    const numTwo = parseInt(b);
+            console.log(typeof numTwo);
+     
+    if(numOne < numTwo) {
+        return `less`;
+    } else if(numOne > numTwo) {
+        return `greater`;
+    } else {
+        return `equal`
+    }      
+    
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));
