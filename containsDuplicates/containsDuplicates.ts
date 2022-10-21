8707fa8d573484b1a346470cb89dd92e718e1b04
Larocca <Larocca@RIVPRO1.net>
2022-10-21 12:24:20 -0400

    Solved Contains Duplicates

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,10 @@
 export function containsDuplicates(a: number[]): boolean {
 
+    const uniqueNumbers: number[] = [...new Set(a)];
+            console.log(uniqueNumbers);
+
+    return a.length !== uniqueNumbers.length ? true : false;       
 }
 
-// console.log(containsDuplicates([1, 2, 3, 1]));
-// console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates([1, 2, 3, 1]));
+console.log(containsDuplicates([3, 1]));
