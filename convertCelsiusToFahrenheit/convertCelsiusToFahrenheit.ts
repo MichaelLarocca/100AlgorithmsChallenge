69b3fc98125d50298db656151b1bae324de86c47
Larocca <Larocca@RIVPRO1.net>
2022-10-24 15:49:43 -0400

    Solved Convert Celsius To Fahrenheit

--- convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
+++ convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
@@ -1,7 +1,11 @@
 export function celsiusToFahrenheit(celsius: number): number {
 
+    const fahrenheit: number = (celsius * 9 / 5) + 32;
+            console.log(fahrenheit);
+
+    return fahrenheit;        
 }
 
-// console.log(celsiusToFahrenheit(-30));
-// console.log(celsiusToFahrenheit(-10));
-// console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(-30));
+console.log(celsiusToFahrenheit(-10));
+console.log(celsiusToFahrenheit(0));
