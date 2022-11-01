6eb07f328200935b0bef8e7c218000cc423c838e
Larocca <Larocca@RIVPRO1.net>
2022-11-01 12:05:34 -0400

    Solved Election Winners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,18 @@
 export function electionsWinners(votes: number[], k: number): number {
 
+    let counterCandidates: number = 0;
+    let leadingCandidate: number = Math.max(...votes);
+            console.log(leadingCandidate);
+
+    for(let i = 0; i < votes.length; i++) {
+            console.log(votes[i] + k);
+
+        if(votes[i] + k> leadingCandidate) {
+            counterCandidates += 1;
+        }
+    }
+            console.log(counterCandidates);
+    return counterCandidates;
 }
 
-// console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 3));
