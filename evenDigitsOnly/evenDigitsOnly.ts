caf14c8b2a7fe6f759d976f31b3a59a1bca4b595
Larocca <Larocca@RIVPRO1.net>
2022-11-04 12:21:38 -0400

    Solved Even Digits Only

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,20 @@
 export function evenDigitsOnly(n: number): boolean {
+            console.log(n);
 
+    let stringNumbers: any[] = n.toString().split('');
+            console.log(stringNumbers)
+            
+    for(let i = 0; i < stringNumbers.length; i++) {
+        console.log(stringNumbers[i]);
+
+        if(stringNumbers[i] % 2 !== 0) {
+                console.log(stringNumbers[i] % 2 !== 0);
+                console.log(stringNumbers[i]);
+            return false;
+        }
+    }  
+    return true;      
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));
