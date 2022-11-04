736caead4650a00e6347ed1224a304e0d3d3abf1
Larocca <Larocca@RIVPRO1.net>
2022-11-04 14:38:18 -0400

    Solved Factorialize A Number

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,19 @@
 export function factorializeANumber(num: number): number {
 
+    let arrayTotal: number[] = [];
+
+    for(let i = 0; i < num; i++) {
+        arrayTotal.push(i+1);
+    }
+        console.log(arrayTotal);
+
+    let sum: number = arrayTotal.reduce((total, currentValue) => {
+        return total * currentValue;
+    }, 1)   
+
+        console.log(sum);
+    return sum;
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
