0ba59e974a8813c2587c9793a903d96f725ae21e
Larocca <Larocca@RIVPRO1.net>
2022-11-04 15:50:24 -0400

    Solved Fancy Ride

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,15 +1,16 @@
 export function fancyRide(l: number, fares: number[]): string {
 
+    let coupon: number = 20;    
     let ride: string[] = [ "UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
     let bestRide: string = ``;
 
     for(let i = fares.length -1; i >= 0; i--) {
             console.log(i);
-            console.log(fares[i] * l - 20);
+            console.log(fares[i] * l - coupon);
 
-        if(fares[i] * l - 20 <= 0) {
+        if(fares[i] * l - coupon <= 0) {
                 console.log(ride[i]);
-                
+
             bestRide = ride[i];
             break;
         }
