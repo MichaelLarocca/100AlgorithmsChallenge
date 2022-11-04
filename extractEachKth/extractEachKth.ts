b2d233b3674deae49978d0e227e41ce1c29caf68
Larocca <Larocca@RIVPRO1.net>
2022-11-04 12:55:17 -0400

    Solved Extract Each Kth

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,19 @@
 export function extractEachKth(inputArray: number[], k: number): number[] {
+                console.log(3 % k);
+                console.log(6 % k);
+                console.log(9 % k);
+
+        let arrayReturn: number[] = [];
+
+        for(let i = 0; i < inputArray.length; i ++) {
+                console.log(inputArray[i] % k);
+
+            if(inputArray[i] % k !== 0) {
+                arrayReturn.push(inputArray[i]);
+            }
+        }
+                console.log(arrayReturn);
+        return arrayReturn;    
 }
 
-// console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
