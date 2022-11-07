a0d332a54b8534333a07f660a1ded04d3d41e4ff
Larocca <Larocca@RIVPRO1.net>
2022-11-07 15:43:29 -0500

    Solved Growing Plant

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,22 @@
 export function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
 
+    let total: number = 0;
+    let counter: number = 0;
+
+    do {
+        if(total != desiredHeight) {
+            total += upSpeed;
+        }
+        if(total != desiredHeight) {
+            total -= downSpeed;
+        }
+        counter += 1;
+            console.log(total);
+            console.log(counter);
+    }
+    while (total < desiredHeight);
+
+    return counter;
 }
 
-// console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910))
