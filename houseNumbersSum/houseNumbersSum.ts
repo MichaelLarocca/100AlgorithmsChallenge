abead5abd3fb008ad0d5731b1c0927dda55ccf4b
Larocca <Larocca@RIVPRO1.net>
2022-11-08 13:23:35 -0500

    Solved House Numbers Sum

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,20 @@
 export function houseNumbersSum(inputArray: number[]): number {
+    
+    let total: number = 0;
+    let flag: boolean = false;
 
+    for(let i = 0; i < inputArray.length; i++) {
+
+        if(inputArray[i] === 0) {
+            flag = true;
+        }
+
+        if(flag === false) {
+            total +=  inputArray[i];
+        }
+    }
+        console.log(total);
+    return total;
 }
 
-// console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
