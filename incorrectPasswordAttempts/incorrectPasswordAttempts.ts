eec503b204365bf82db3d2f57d8f45d87ee99452
Larocca <Larocca@RIVPRO1.net>
2022-11-08 14:23:01 -0500

    Solved Incorrect Password Attempts

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,25 @@
 export function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+    
+    let attemptsNumber: number = 0;
+    let passcodeFailed: boolean = false;
 
+    for(let i = 0; i < attempts.length; i++) {
+        console.log(attempts[i]);
+
+        if(passcode === attempts[i]) {
+            attemptsNumber = 0;
+        }
+
+        if(attemptsNumber !== 10) {
+            attemptsNumber += 1;
+        }
+
+        if(attemptsNumber === 10) {
+            passcodeFailed = true;
+        }
+    }
+        console.log(passcodeFailed);
+    return passcodeFailed;
 }
 
-// console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
