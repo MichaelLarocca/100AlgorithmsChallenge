afaf861e02f6071a10e7796ed85079a16a293b0b
Larocca <Larocca@RIVPRO1.net>
2023-01-04 15:31:10 -0500

    Solved isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,24 @@
 export function isTandemRepeat(inputString: string): boolean {
+    console.log(inputString);
+
+    if(inputString.length % 2 === 0) {
+        const sliceStringFirstHalf = inputString.slice(0,inputString.length / 2);
+                console.log(sliceStringFirstHalf);
+        const sliceStringSecondHalf = inputString.slice(inputString.length / 2);   
+                console.log(sliceStringSecondHalf);
+
+        if( sliceStringFirstHalf === sliceStringSecondHalf) {
+            return true;
+        } else {
+            return false;
+        }     
+    } else {
+        return false;
+    }
+
 
 }
 
-// console.log(isTandemRepeat('tandemtandem'))
-// console.log(isTandemRepeat('qqq'))
-// console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'))
+console.log(isTandemRepeat('qqq'))
+console.log(isTandemRepeat('2w2ww'))
