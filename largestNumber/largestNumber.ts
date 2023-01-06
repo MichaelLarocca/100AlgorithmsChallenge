3253a2546bf6de2f0055fbb00a5d6d7f5bc1b1a4
Larocca <Larocca@RIVPRO1.net>
2023-01-06 11:29:24 -0500

    Solved largestNumber

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,11 @@
 export function largestNumber(n: number): number {
+    let outPut: string = "";
 
+    for(let i = 0; i < n; i++) {
+        outPut += "9";
+    }
+        console.log(outPut);
+    return parseInt(outPut);
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(2));
