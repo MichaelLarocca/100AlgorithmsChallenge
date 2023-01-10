f95380ff445efc4779a4abeaf637b8e0f46a822d
Larocca <Larocca@RIVPRO1.net>
2023-01-10 15:42:08 -0500

    Solved makeArrayConsecutive2

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,22 @@
 export function makeArrayConsecutive2(statues: number[]): number {
+    
+    const numbers: number[] = [];  
+    let counter: number = 0;
 
+    const sortedNumbers = statues.sort((a,b)=>a-b);
+        console.log(sortedNumbers);
+
+    for(let i = sortedNumbers[0]; i < sortedNumbers[sortedNumbers.length-1]; i++) {
+        numbers.push(i);
+    } 
+        console.log(sortedNumbers)
+        console.log(numbers)
+
+    numbers.forEach(num => {
+        sortedNumbers.includes(num) ? counter += 1 : counter;
+    })    
+            console.log(counter);
+        return counter;
 }
 
-// console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([6, 2, 3, 8]));
