5aa1204ffc126f0e9dfc632697ef62a543f5d6cd
Larocca <Larocca@RIVPRO1.net>
2023-01-17 17:23:45 -0500

    Solved newNumeralSystem

--- newNumeralSystem/newNumeralSystem.ts
+++ newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,22 @@
 export function newNumeralSystem(number: string): string[] {
+    const letterArray: string[] = [];
 
+    let letterA: string = String.fromCharCode(65);
+        console.log(letterA);
+
+    let start: number = 65;
+
+    let current: number = number.charCodeAt(0);
+        console.log(current);
+
+    while (String.fromCharCode(start) !== String.fromCharCode(current)) {
+        letterArray.push(`${String.fromCharCode(start)} + ${String.fromCharCode(current)}`);
+        start += 1;
+        current -= 1;
+    }
+    letterArray.push(`${String.fromCharCode(start)} + ${String.fromCharCode(current)}`);
+
+        console.log(letterArray);
+    return letterArray;
 }
-
-// console.log(newNumeralSystem('G'));
+console.log(newNumeralSystem('G'));
